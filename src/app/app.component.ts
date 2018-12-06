bf5fad10c473cd6f4688e2a154479bb7dfb5c0bd
Dan Bock <danbock@gmail.com>
2018-12-06 16:33:23 -0500

    Add Firebase plugin with Crashlytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+const firebase = require("nativescript-plugin-firebase");
 
 @Component({
     moduleId: module.id,
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    ngOnInit() {
+        firebase.init({}).then(
+            instance => console.log('firebase.init done'),
+            error => console.log(`firebase.init error: ${error}`)
+        );
+    }
+}
