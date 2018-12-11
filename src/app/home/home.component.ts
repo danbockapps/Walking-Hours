3bbb7cef2936ce8292c0acf3d12951e37514b7f3
Dan Bock <danbock@gmail.com>
2018-12-10 22:01:30 -0500

    New StepHoursHash interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import { PedometerService } from "../services/pedometer.service";
 import { on, suspendEvent, resumeEvent, ApplicationEventData } from 'tns-core-modules/application';
 import { Subscription, interval } from "rxjs";
 import { PedometerUpdate } from "nativescript-pedometer";
+import * as _ from "lodash";
 
 @Component({
   selector: "Home",
@@ -12,8 +13,7 @@ import { PedometerUpdate } from "nativescript-pedometer";
   templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
-  // This is an array of tuples. Google TypeScript tuples.
-  stepHours: Array<[Moment, number]>;
+  stepHours: StepHoursHash;
   private updatesSubscription: Subscription;
   curViewDate: Date; // Midnight of the date currently being viewed
 
@@ -21,10 +21,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.curViewDate = moment().startOf('day').toDate();
-    this.stepHours = [];
+    this.stepHours = {};
     this.pedometerService.queryDay(moment()).subscribe(
       (response: PedometerUpdate) => {
-        this.stepHours.push([moment(response.startDate), response.steps]);
+        this.stepHours[moment(response.startDate).format('X')] = response.steps;
       }
     );
 
@@ -41,15 +41,9 @@ export class HomeComponent implements OnInit {
   // Adds steps from a PedometerUpdate into the stepsHours property
   // Doing it this way with the = and the => makes it use the right "this".
   collectPedometerUpdate = (resp: PedometerUpdate) => {
-    if(this.stepHours) {
-      this.stepHours.forEach((element: [Moment, number]) => {
-        if(element[0].isSame(moment(resp.startDate))) {
-          element[1] = resp.steps;
-          console.log(`Steps received: ${resp.steps}`);
-          this.changeDetectorRef.detectChanges();
-        }
-      });
-    }
+    this.stepHours[moment(resp.startDate).unix()] = resp.steps;
+    console.log(`Steps received: ${resp.steps}`);
+    this.changeDetectorRef.detectChanges();
   }
 
   unsubscribeFromPedometerUpdates(): void {
@@ -67,9 +61,18 @@ export class HomeComponent implements OnInit {
   }
 
   todaysStepHours(curViewDate): Array<[Moment, number]> {
-    return this.stepHours.filter((element: [Moment, number]) => {
-      return moment(element[0]).startOf('day').isSame(curViewDate);
-    });
+    // https://blog.mariusschulz.com/2015/05/14/implicit-function-chains-in-lodash
+    // pickBy filters this.stepHours down to only the elements in curViewDate
+    // mapValues returns an obj with both key (moment) and value (steps) in the value
+    // values turns that into an array of tuples
+    // value is the explicit end of the method chain.
+    const returnable: any = _(this.stepHours).pickBy(
+      (value: number, key: string) => moment.unix(parseInt(key)).startOf('day').isSame(curViewDate)
+    ).mapValues(
+      (value: number, key: string) => [moment.unix(parseInt(key)), value] // This is called a tuple.
+    ).values().value();
+
+    return returnable;
   }
 
   rowNum(stepHour: [Moment, number]): number {
@@ -81,4 +84,12 @@ export class HomeComponent implements OnInit {
     // Columns 0 and 2 are for hours. Column 1 is the center divider.
     return stepHour[0].format('a') === 'am' ? 0 : 2;
   }
+}
+
+interface StepHoursHash {
+  // Index is the unix timestamp of the start of an hour.
+  // Value is the number of steps for that hour.
+
+  // This is called an "index signature".
+  [index: number]: number;
 }
